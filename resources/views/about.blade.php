d27e00bb4c4983c75ef141800c03bf7990758888
osamcoderz <dipika.letsnurture@gmail.com>
2023-10-06 04:47:41 +0530

    job filter

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,19 @@
 @extends('user.layouts.authapp')
 @section('content')
 
-
+<style>
+    .step-card {
+    background: #fff;
+    padding: 30px;    
+    z-index: 3;
+    border-radius: 15px;
+}
+</style>
 <section class="form-section about-section">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="step-info-card">
+                    <div class="step-card">
                         <div class="about-card-box">
                             @if($about && $about->page_title)
                             <h1>{!! $about->page_title !!} </h1>
--- resources/views/termsPrivacy.blade.php
+++ resources/views/termsPrivacy.blade.php
@@ -1,10 +1,21 @@
 @extends('user.layouts.authapp')
 @section('content')
+
+
+<style>
+    .step-card {
+    background: #fff;
+    padding: 30px;    
+    z-index: 3;
+    border-radius: 15px;
+}
+</style>
+
 <section class="form-section about-section">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="step-info-card">
+                    <div class="step-card">
                         <div class="about-card-box">
                             @if($privacy && $privacy->page_title)
                             <h1>{!! $privacy->page_title !!} </h1>
